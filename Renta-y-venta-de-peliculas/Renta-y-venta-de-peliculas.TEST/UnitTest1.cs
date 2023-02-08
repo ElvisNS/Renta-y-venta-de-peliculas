e8d3d53efdf817746aacef4d241b20f3621ecb57
ElvisNS <121916928+ElvisNS@users.noreply.github.com>
2023-02-07 22:34:34 -0400

    Primera Subida

---
+++ Renta-y-venta-de-peliculas/Renta-y-venta-de-peliculas.TEST/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Renta_y_venta_de_peliculas.TEST
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Renta-y-venta-de-peliculas/Renta-y-venta-de-peliculas.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Renta_y_venta_de_peliculas.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
