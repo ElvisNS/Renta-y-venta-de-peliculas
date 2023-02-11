d40833d69afbc6ab4e73e4cd92c751da401e904e
꧁Wasthanael Encarnación꧂ <wasthanael@gmail.com>
2023-02-11 10:19:18 -0400

    Creacion del controlador, model y view de genero

---
+++ Renta-y-venta-de-peliculas/Renta-y-venta-de-peliculas.Web/Controllers/GenerosController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Renta_y_venta_de_peliculas.Web.Controllers
+{
+    public class GenerosController : Controller
+    {
+        // GET: GenerosController
+        public ActionResult Index()
+        {
+            List<Models.GenerosModels> Generos = new List<Models.GenerosModels>()
+            {
+                new Models.GenerosModels
+                {
+                    cod_genero = 12, txt_desc="Terror"
+                },
+                new Models.GenerosModels
+                {
+                    cod_genero = 12, txt_desc="Accion"
+                },
+                new Models.GenerosModels
+                {
+                    cod_genero = 12, txt_desc="Romance"
+                },
+                new Models.GenerosModels
+                {
+                    cod_genero = 12,
+                    txt_desc = "Ciencia Ficcion"
+                }
+            };
+            return View(Generos);
+        }
+
+        // GET: GenerosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: GenerosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GenerosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GenerosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: GenerosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GenerosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: GenerosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Renta-y-venta-de-peliculas/Renta-y-venta-de-peliculas.Web/Models/GenerosModels.cs
@@ -0,0 +1,8 @@
+﻿namespace Renta_y_venta_de_peliculas.Web.Models
+{
+    public class GenerosModels
+    {
+        public int cod_genero { get; set; }
+        public string? txt_desc { get; set; } 
+    }
+}
