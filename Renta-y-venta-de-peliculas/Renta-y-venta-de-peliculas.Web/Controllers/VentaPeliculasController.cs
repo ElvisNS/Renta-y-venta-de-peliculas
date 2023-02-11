403269cf60a920f71c151f9f8ab78c6471fedaff
Yendry Ramon Antonio Mejia Garcia <yendry3124@gmail.com>
2023-02-11 12:34:28 -0400

    Creacion del controller, model y view de  venta de peliculas

---
+++ Renta-y-venta-de-peliculas/Renta-y-venta-de-peliculas.Web/Controllers/VentaPeliculasController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Renta_y_venta_de_peliculas.Web.Controllers
+{
+    public class VentaPeliculasController : Controller
+    {
+        // GET: VentaPeliculasController
+        public ActionResult Index()
+        {
+            List<Models.VentaPeliculasModel> VPeliculas = new List<Models.VentaPeliculasModel>() 
+            { 
+                new Models.VentaPeliculasModel() 
+                {
+                    Id= 1,cod_usuario=01, cod_pelicula=08, precio=8786, fecha = new DateTime(2023, 2, 10)
+                },
+                new Models.VentaPeliculasModel()
+                {
+                    Id= 2,cod_usuario=09, cod_pelicula=02, precio=250, fecha = new DateTime(2023, 7, 10)
+                },
+                new Models.VentaPeliculasModel()
+                {
+                    Id= 3,cod_usuario=06, cod_pelicula=03, precio=300, fecha = new DateTime(2023, 8, 10)
+                },
+                new Models.VentaPeliculasModel()
+                {
+                    Id= 4,cod_usuario=05, cod_pelicula=05, precio=150, fecha = new DateTime(2023, 6, 10)
+                }
+            };
+            return View(VPeliculas);
+        }
+
+        // GET: VentaPeliculasController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: VentaPeliculasController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VentaPeliculasController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VentaPeliculasController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: VentaPeliculasController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VentaPeliculasController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: VentaPeliculasController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Renta-y-venta-de-peliculas/Renta-y-venta-de-peliculas.Web/Models/VentaPeliculasModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Renta_y_venta_de_peliculas.Web.Models
+{
+    public class VentaPeliculasModel
+    {
+        public int Id { get; set; }
+        public int cod_pelicula { get; set; }
+        public int  cod_usuario { get; set; }
+        public decimal precio { get; set; } 
+        public  DateTime fecha { get; set; }
+    }
+}
