c4ec07be8305c10052086446b97712c4ee66b957
abranchzorr@gmail.com <abranchzorr@gmail.com>
2023-03-25 12:05:21 -0400

    Implementacion de la capa de servicios

--- Renta-y-venta-de-peliculas/Renta-y-venta-de-peliculas.DAL/Core/AuditEntity.cs
+++ Renta-y-venta-de-peliculas/Renta-y-venta-de-peliculas.DAL/Core/AuditEntity.cs
@@ -10,7 +10,7 @@ namespace Renta_y_venta_de_peliculas.DAL.Core
         }
         public int Create_user { get; set; }
         public DateTime Create_date { get; set; }
-        public int Modify_user { get; set; }
+        public int? Modify_user { get; set; }
         public DateTime? Modify_date { get; set; }
         public int? Deleted_user { get; set; }
         public DateTime? Deleted_date { get; set; }
--- Renta-y-venta-de-peliculas/Renta-y-venta-de-peliculas.DAL/Entities/Pelicula.cs
+++ Renta-y-venta-de-peliculas/Renta-y-venta-de-peliculas.DAL/Entities/Pelicula.cs
@@ -10,7 +10,7 @@ namespace Renta_y_venta_de_peliculas.DAL.Entities
     {
        [Key]
         public int Cod_pelicula { get; set; }
-        public string Txt_desc { get; set; } = null!;
+        public string Txt_desc { get; set; }
         public int Cant_disponibles_alquiler { get; set; }
         public int Cant_disponibles_venta { get; set; }
         public decimal Precio_alquiler { get; set; }
